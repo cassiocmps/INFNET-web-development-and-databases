540580cca55f92c50f79e956eabcf0ddf6bba920
cassiocmps <cassio.szcampos@gmail.com>
2025-09-04 21:41:06 -0300

    [DR4_TP2] Exercise 3 - Server-side Validation with Data Annotations

---
+++ DR4_TP2/Pages/Ex03_ServerValidation.cshtml.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace DR4_TP2.Pages
+{
+    public class Ex03_ServerValidationModel : PageModel
+    {
+        [BindProperty]
+        public InputModel Input { get; set; } = new();
+
+        public bool IsSubmitted { get; set; } = false;
+
+        public class InputModel
+        {
+            [Required(ErrorMessage = "City name is required.")]
+            [MinLength(3, ErrorMessage = "City name must be at least 3 characters long.")]
+            public string CityName { get; set; } = string.Empty;
+        }
+
+        public void OnGet() { }
+
+        public IActionResult OnPost()
+        {
+            IsSubmitted = true;
+
+            if (ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            return Page();
+        }
+    }
+}
